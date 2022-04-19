db9939af81acf6856e38c1a042710da3abea793c
akkradet <akkradet7@gmail.com>
2022-04-19 13:35:49 +0700

    auto commit and push on 2022-19-04_13-35-48

--- git.bat
+++ git.bat
@@ -5,6 +5,6 @@ set d=%t:~10,4%-%t:~7,2%-%t:~4,2%_%t:~15,2%-%t:~18,2%-%t:~21,2%
 
 git.exe add .
 
-git.exe commit -am "auto backup commit and push on %d%"
+git.exe commit -am "auto commit and push on %d%"
 
-git.exe push
+git.exe push origin master
