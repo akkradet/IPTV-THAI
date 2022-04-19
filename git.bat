50574c64ba2c96cf0a6816217936718db50a797d
akkradet <akkradet7@gmail.com>
2022-04-19 13:20:57 +0700

    auto backup commit and push on 2022-19-04_13-20-57

--- git.bat
+++ git.bat
@@ -3,8 +3,8 @@ cd D:\Git\IPTV-THAI
 set t=%date%_%time%
 set d=%t:~10,4%-%t:~7,2%-%t:~4,2%_%t:~15,2%-%t:~18,2%-%t:~21,2%
 
-git add .
+git.exe add .
 
-git commit -am "auto backup commit and push on %d%"
+git.exe commit -am "auto backup commit and push on %d%"
 
-git push
+git.exe push
