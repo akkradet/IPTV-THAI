78150aa9f3d582befbb6f59dc16a4372487d816a
akkradet <akkradet7@gmail.com>
2022-04-20 11:55:55 +0700

    auto commit and push on 2022-04-20_11-55-55

--- git.bat
+++ git.bat
@@ -1,7 +1,7 @@
 @echo off
 cd D:\Git\IPTV-THAI
 set t=%date%_%time%
-set d=%t:~10,4%-%t:~7,2%-%t:~4,2%_%t:~15,2%-%t:~18,2%-%t:~21,2%
+set d=%t:~10,4%-%t:~4,2%-%t:~7,2%_%t:~15,2%-%t:~18,2%-%t:~21,2%
 
 git.exe add .
 
